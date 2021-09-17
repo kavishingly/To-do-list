1777d788b2ff1aa16fa56740260c5cbb6bdcd20d
Kavish Agrawal <Kavish29101999@gmail.com>
2021-09-17 13:28:38 -0700

    Added database, single route

--- app.js
+++ app.js
@@ -1,45 +1,88 @@
 //dummy commit
-const express = require("express")
-const bodyParser = require("body-parser")
-const date = require(__dirname + "/date.js");
+const express = require("express");
+const Mongoose = require("mongoose");
+
 const app = express();
-const items = ["Buy food", "Eat food", "Sleep"]; //array of items
-const workItems = ["Meeting at 4pm", "Study", "Sleep"]; //array of work-items
-app.use(bodyParser.urlencoded({ extended: true }));
+Mongoose.connect("mongodb://localhost:27017/todolistDB")
+
+const itemsSchema={
+    
+    name: String
+};
+
+const Item=Mongoose.model("Item",itemsSchema); 
+//Item means collection named as "items"
+//const <model name>=Mongoose.model(<collection name>,<schema name>)
+
+const item1=new Item({
+    name: "welcome to to-do-list"
+})
+
+const item2=new Item({
+    name: "welcome again to to-do-list"
+})
+
+const item3=new Item({
+    name: "welcome again to to-do-list"
+})
+const defaultItems=[item1,item2,item3];
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public")); //to serve static files
 app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => { //1. server will give home page as response to get request by browser
-    /*
-    date(); means run the functions that are bound to date module*/
-    // let day=date.getDay(); -----to print only Day
-    let day = date.getDate(); // to print full date
-    res.render('list',
-        {
-            title: day,
-            items: items
-        }); //this will load the list.ejs and pass params title and items array
-
+app.get("/", (req, res) => {
+    Item.find({},(err,foundItems)=>{
+        //  console.log("Length is "+foundItems.length);
+        if(foundItems.length ==0){
+            Item.insertMany(defaultItems,(err)=>{
+                if(err){
+                    console.log(err);
+                }else{
+                    console.log("Added items successfully");
+                }
+            })
+            res.redirect("/")
+        }else{
+        res.render("list",{title:"Today",newListItems:foundItems}) //---important: rendering foundItems array from db
+        }
+    })  
+    
 });
-app.get("/work", (req, res) => {
 
-    res.render("list", { title: "Work list", items: workItems })
-    //when get request to work route is made, it renders list.ejs with above params
+
+//-----------------inserting element-----------------------------
+app.post('/', (req, res) => {
+    
+    const itemName = req.body.newItem //itemName is name entered by user in text input
+    const newItemEntered=new Item({ //creating new row
+        name:itemName
+    })
+    newItemEntered.save(); //saving this row to our db and redirectiong to home route
+    res.redirect("/")
+    // deleting work route to simplifiy things as of now
 })
-app.post('/', (req, res) => { //2. when submit btn is hit, form POST the data at home route
-    const item = req.body.newItem
-    /*here newItem is value name attribute in input tag
-    console.log(req.body); it returns an object eg { newItem: 'input text', listType: 'Work' }
-    now when submit btn is hit, if the page was Day then listType='thursday',
-        and if page was Work list then listType="Work" */
-    if (req.body.listType === "Work") {
-        workItems.push(item);
-        res.redirect("/work"); //listType="Work" then push item to work array and load work route
-    } else {
-        items.push(item)
-        res.redirect("/"); //else push item to items array and load home route
 
-    }
+//-----------------deleting element-----------------------------
+/*steps:
+-wrap the checkbox and item div inside form tag which post at route /delete
+-submit form when checkbox is clicked using onChange attribute in checkbox
+-each row in db has a unique id,access this _id by adding it in value attribute 
+-now delete the item which has above _id using findByIdAndRemove() function in mongoose
+*/
+app.post("/delete",(req,res)=>{
+    // console.log(req.body.checkBox);
+    const deletedId=req.body.checkBox;
+
+    Item.findByIdAndRemove(deletedId,(err)=>{
+        if(err){
+            console.log(err);
+        }else{
+            console.log("Deleted successfully");
+        }
+    })
+    res.redirect("/")
+    
 })
 
 app.listen(3000, () => {
